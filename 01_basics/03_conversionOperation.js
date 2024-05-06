453538a5a0645cf56c4cd0d2af0633bae1142810
Hirdesh Mewada <69684968+hirdeshmewada@users.noreply.github.com>
2024-05-06 13:45:18 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,44 @@
+// let score=33
+// let score2="33abc"
+// let score3=null
+// let score4=true
+// let score5="Hitesh"
+
+// console.log(typeof score);
+// console.log(typeof (score2));
+
+// let valueInNumber=Number(score2)
+// let valueInNumber1=Number(score3)
+// let valueInNumber4=Number(score4)
+// let valueInNumber5=Number(score5)
+
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// console.log(valueInNumber1);
+
+// console.log(valueInNumber4);
+// console.log(valueInNumber5);
+
+
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1;false=>0
+//
+
+// let isLoggedIn=" "
+// let booleanIdLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIdLoggedIn);
+
+//1=>true;//0=>false
+//"hitesh"=>true
+
+
+let someNumber=33
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+//browser gives string we convert it to string or number assray
