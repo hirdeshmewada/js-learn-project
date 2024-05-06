38c2d3d4b351a6f77a59a8bb8c873a4815fbf26d
Hirdesh Mewada <69684968+hirdeshmewada@users.noreply.github.com>
2024-05-06 14:38:53 +0000

    conversion v

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,16 +1,16 @@
-// let score=33
-// let score2="33abc"
-// let score3=null
-// let score4=true
-// let score5="Hitesh"
+let score=33
+let score2="33abc"
+let score3=null
+let score4=true
+let score5="Hitesh"
 
 // console.log(typeof score);
 // console.log(typeof (score2));
 
-// let valueInNumber=Number(score2)
-// let valueInNumber1=Number(score3)
-// let valueInNumber4=Number(score4)
-// let valueInNumber5=Number(score5)
+let valueInNumber=Number(score2)
+let valueInNumber1=Number(score3)
+let valueInNumber4=Number(score4)
+let valueInNumber5=Number(score5)
 
 
 // console.log(typeof valueInNumber);
@@ -27,8 +27,8 @@
 //true=>1;false=>0
 //
 
-// let isLoggedIn=" "
-// let booleanIdLoggedIn=Boolean(isLoggedIn)
+let isLoggedIn=" "
+let booleanIdLoggedIn=Boolean(isLoggedIn)
 // console.log(booleanIdLoggedIn);
 
 //1=>true;//0=>false
@@ -37,8 +37,49 @@
 
 let someNumber=33
 let stringNumber=String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 
 //browser gives string we convert it to string or number assray
+// why string to numbr is confusing 
+let value =3
+let negValue =-value
+// console.log(negValue)
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(20%3);
+// console.log(4-3);
+
+let str1="Hello "
+let str2="Hirdesh"
+let str3=str1+str2
+// console.log(str3); //Hello Hirdesh
+
+
+// console.log("1"+2);  //=12
+// console.log(1+"2");   //=12
+// console.log("1"+2+2);  //=122
+// console.log(1+2+"2");  //=32
+// console.log(1+2*5%3);  //=2 //in exams there areimp but practically they are not important
+// //this is not the right way to write code use brackets 
+// console.log( (1+20)*20%3);
+
+// console.log(+true);   //1
+// console.log(+"");   //0
+// console.log(true+);   //error
+
+
+let num1,num2,num3
+num1=num2=num3=2+2  //donot write confusing code use code that is readable 
+
+let gameCount=100
+gameCount++;
+console.log(gameCount);  //101
+++gameCount;
+console.log(++gameCount);//103
+console.log(gameCount++);//103
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
