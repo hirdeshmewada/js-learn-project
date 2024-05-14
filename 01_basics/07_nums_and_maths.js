2bfd97e75a3c86cd910125af863824b356468b98
hirdeshmewada <djhd221@gmail.com>
2024-05-14 13:46:59 +0530

    day11

--- 01_basics/07_nums_and_maths.js
+++ 01_basics/07_nums_and_maths.js
@@ -1,17 +1,41 @@
 const score=400
 const balance =new Number(100);
-console.log(score);
-console.log(balance);
+// console.log(score);  //400
+// console.log(balance);   //[Number:100]
 
 
-console.log(balance.toString());
-console.log(balance.toString().length());
-console.log(balance.toFixed(2));
-
+// console.log(balance.toString());  //100
+// console.log(balance.toString().length); //3
+// console.log(balance.toFixed(2));  //100.00
+ 
 const otherNumber=123.8932
-console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toPrecision(4));  //123.9
 
 const hundreds=100000;
-console.log(hundreds.toLocaleString('en=IN'));
+// console.log(hundreds.toLocaleString('en-IN'));   //1,00,000
+
+//*************MAths************************ */
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));  //4
+// console.log(Math.round(4.34));  //4
+// console.log(Math.ceil(4.34));  //5
+// console.log(Math.floor(4.94));  //4
+
+
+// console.log(Math.min(4,3,6,7,3,24,5,5,2));  //2
+// console.log(Math.max(4,3,6,7,3,24,5,5,2));   //24
+
+console.log(Math.random());  //0-1 random values 
+
+console.log(Math.random()*100);  //33.424242
+console.log(Math.random()*10);  //2.34.2342
+console.log(Math.floor(Math.random()*10)+1);  //2  floor value 
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)  //12/13//15//18//14
 
 
