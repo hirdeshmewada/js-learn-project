76db790d50267bbe250d2167e1345cbd35e4ad4c
hirdeshmewada <djhd221@gmail.com>
2024-06-12 15:53:03 +0530

    project 5 and 6

---
+++ 07_projects/6-unlimitedColors/chaiaurcode.js
@@ -0,0 +1,28 @@
+// generate a random color
+
+const randomColor = function () {
+  const hex = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+let intervalId;
+// console.log(randomColor())
+const startChangingColor = function () {
+  if (!intervalId) {
+    intervalId = setInterval(ChangeBgColor, 1000);
+  }
+  function ChangeBgColor() {
+    document.body.style.backgroundColor = randomColor();
+  }
+};
+const stopChangingColor = function () {
+  clearInterval(intervalId);
+  intervalId = null;
+};
+
+document.querySelector("#start").addEventListener("click", startChangingColor);
+
+document.querySelector("#stop").addEventListener("click", stopChangingColor);
